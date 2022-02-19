d467896471351c5ab456c61fa579a3df216f6822
Konstantin Zhiliaev <k926227@gmail.com>
2022-02-19 14:32:16 +0300

    refactor: change url parse method

--- middleware/parseUrl.js
+++ middleware/parseUrl.js
@@ -1,1 +1,1 @@
-module.exports = (baseUrl) => (req, res) => {  const parsedUrl = new URL(req.url, baseUrl)  const params = {}  parsedUrl.searchParams.forEach((value, key) => params[key] = value)  req.pathname = parsedUrl.pathname  req.params = params}
+module.exports = (baseUrl) => (req) => {  const parsedUrl = new URL(req.url, baseUrl)  const params = {}  parsedUrl.searchParams.forEach((value, key) => params[key] = value)  req.pathnameParts = parsedUrl.pathname.match(/\/\w+/g)  req.params = params}
